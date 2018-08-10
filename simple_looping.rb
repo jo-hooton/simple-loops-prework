02515e22f1deacca955bcf5e2e1b35929d32a155
Jo Hooton <joanneelizabethhooton@gmail.com>
2018-08-10 19:25:17 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -36,7 +36,8 @@ end
 
 
 def for_iterator(number_of_times)
-    for number_of_times in 1..7 do
+  number_of_times = 1..7
+    for number_of_times do
       puts "Welcome to Flatiron School's Web Development Course!"
     end
 end
